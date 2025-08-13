9654c0b8630e93bf3a04f24ea560cd98794de919
adraj2023 <ad.rajkoradiya@gmail.com>
2025-08-13 09:10:26 +0000

    add variables

---
+++ variables.js
@@ -0,0 +1,23 @@
+const accountId = 12345
+let accountEmail = "raj@gamil.com"
+var accountPassword = "98765"
+accountCity = "Surat" // not good to use
+let accountState; // this is allowd in let (decraration is not complosury for let)
+
+// const accountState; this is NOT allowd in let (decraration is complosury for const)
+// accountId = 2
+    // assinment is not allowed in const
+
+
+accountEmail = "test@gmail.com"
+accountPassword = "546"
+accountCity = "Rajkot"
+
+
+// "let" is block scop
+
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
