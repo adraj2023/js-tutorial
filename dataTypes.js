0b63cfabdfe684e8e9319f7fba7962c438340fe1
adraj2023 <ad.rajkoradiya@gmail.com>
2025-08-13 11:24:06 +0000

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,29 @@
+ "use strict" // trate all JS code as newer version
+
+//  alert(3 + 3);  // We are using node not browser so it not working
+
+// DataTypes
+// 1. Primitive 
+// 2. object
+
+// 1. Primitive  value
+
+// number => range 2 to power
+// bigint
+// string => "" or ''
+// boolean => true/fasle
+// null => standalone value (N/A)
+// Undefined => 
+// symbol => unique
+
+let fname = "Raj"; //string
+let age = 29; //number
+let isLoggedIn = true; // boolean
+let state; //undefined
+let city = null; //null
+
+console.log(typeof age);
+
+console.log(typeof null); // result is object
+
+console.log(typeof undefined); // result is undefined
