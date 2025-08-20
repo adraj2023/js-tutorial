43109395b17387f5c29176f8cc29322bf3a64de5
adraj2023 <ad.rajkoradiya@gmail.com>
2025-08-20 07:04:02 +0000

    number and maths

--- 01_basic/07_nums_and_maths.js
+++ 01_basic/07_nums_and_maths.js
@@ -18,3 +18,14 @@ const hundred = 1000000
 
 // +++++++++++++++++ Maths +++++++++++++++++
 console.log(Math);
+
+console.log(Math.abs(-4)); // convert value in to positive value
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2)); // upper value
+console.log(Math.floor(4.9)); // lower value
+console.log(Math.sqrt(36));
+console.log(Math.pow(2, 4));
+console.log(Math.max(2, 4, 8, 6));
+console.log(Math.min(2, 4, 8, 6));
+
+
